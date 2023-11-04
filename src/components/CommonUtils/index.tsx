5511a21e7a3954b89c1eb86d18ecfbfae6ed8b85
hecticme <hangoc.minh1306@gmail.com>
2023-11-04 23:01:25 +0700

    Add open function for utilities panel
    This added the logic for opening common utilities panel. UI animation and colors have also been added.

--- src/components/CommonUtils/index.tsx
+++ src/components/CommonUtils/index.tsx
@@ -13,18 +13,32 @@ export default function CommonUtils() {
 
   return (
     <div ref={ref} className="relative">
-      <button className="flex gap-2 items-center justify-between font-bold px-3 py-1 bg-gray-900 hover:bg-gray-700 dark:bg-gray-700 dark:hover:bg-gray-600 transition-colors text-gray-100">
+      <button
+        className="group flex gap-2 items-center justify-between font-bold px-3 py-1 bg-gray-900 hover:bg-gray-700 dark:bg-gray-700 dark:hover:bg-gray-600 transition-colors text-gray-100"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
         <span className="flex gap-1 items-center">
           Common utilities
           <WrenchScrewdriverIcon className="w-5 h-5" />
         </span>
 
         <span>
-          <ChevronDownIcon className="w-4 h-4" />
+          <ChevronDownIcon
+            className={`w-5 h-5 transition-[colors,transform] group-hover:bg-gray-500 bg-gray-700 dark:bg-gray-900 dark:group-hover:bg-gray-800 p-1 rounded-full ${
+              open ? 'rotate-180' : ''
+            }`}
+          />
         </span>
       </button>
 
-      <div className="absolute top-[120%] right-0 flex flex-col gap-1 bg-gray-900 dark:bg-gray-700 text-gray-100 px-2 py-1">
+      <div
+        aria-hidden={!open}
+        className={`absolute right-0 flex flex-col gap-1 bg-gray-900 dark:bg-gray-700 transition-[opacity,top] text-gray-100 px-2 py-1 ${
+          open ? 'opacity-100 top-[120%]' : 'opacity-0 top-[110%]'
+        }`}
+      >
         <CurrentTimmeBtn />
       </div>
     </div>
