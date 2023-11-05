590f54ece7f2578903d005455a4ee296508fa429
hecticme <hangoc.minh1306@gmail.com>
2023-11-05 22:02:07 +0700

    Add z-index for some drop-down elements
    Added z-index property for Timezone selector and Converter amount selector, so that they cascade on other elements.

--- src/components/InstancesAmountSelect.tsx
+++ src/components/InstancesAmountSelect.tsx
@@ -62,7 +62,7 @@ export const InstancesAmountSelect = ({
 
         <ul
           aria-hidden={!open}
-          className={`absolute flex flex-col gap-2 bg-gray-900 px-1 py-2 transition-[top,opacity] dark:bg-gray-700 ${
+          className={`absolute z-10 flex flex-col gap-2 bg-gray-900 px-1 py-2 transition-[top,opacity] dark:bg-gray-700 ${
             open
               ? 'top-[120%] opacity-100'
               : 'pointer-events-none top-[110%] opacity-0'
