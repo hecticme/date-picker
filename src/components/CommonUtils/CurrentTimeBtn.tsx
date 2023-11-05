60ee0dd81595c180911265908f15aac1ee3f566a
hecticme <hangoc.minh1306@gmail.com>
2023-11-05 09:06:35 +0700

    Add get current unix button logic and UI
    This added display and get current timestamp function.

--- src/components/CommonUtils/CurrentTimeBtn.tsx
+++ src/components/CommonUtils/CurrentTimeBtn.tsx
@@ -1,3 +1,23 @@
+import { useState } from 'react';
+import dayjs from 'dayjs';
+
 export default function CurrentTimeBtn() {
-  return <button>Get current timestamp</button>;
+  const [timestamp, setTimestamp] = useState(dayjs().unix());
+
+  return (
+    <div className="flex flex-col gap-2">
+      <button
+        className="whitespace-nowrap bg-gray-900 px-3 py-2 font-bold text-gray-100 transition-colors hover:bg-gray-700 dark:bg-gray-700 dark:hover:bg-gray-600"
+        onClick={() => {
+          setTimestamp(dayjs().unix());
+        }}
+      >
+        Get current unix timestamp
+      </button>
+
+      <p className="flex justify-center overflow-hidden text-ellipsis border-2 border-solid border-gray-900 px-2 py-1 font-mono dark:border-gray-200">
+        {timestamp}
+      </p>
+    </div>
+  );
 }
